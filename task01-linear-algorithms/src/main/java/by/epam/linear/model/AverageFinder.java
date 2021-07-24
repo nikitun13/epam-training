f5b1010e4c07c1f86cc93cb7006c33e65ae8844d
Nikita Romanov <nikitun13@gmail.com>
2021-07-24 23:05:12 +0300

    refactor task 5

---
+++ task01-linear-algorithms/src/main/java/by/epam/linear/model/AverageFinder.java
@@ -0,0 +1,11 @@
+package by.epam.linear.model;
+
+public class AverageFinder {
+
+    /*
+     * 5. Create an algorithm for finding the average of two numbers
+     */
+    public double average(double firstNumber, double secondNumber) {
+        return (firstNumber + secondNumber) / 2;
+    }
+}
