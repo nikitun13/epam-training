1813780f4bbba31e06ad0e63bcf9982fa3c10978
Nikita Romanov <nikitun13@gmail.com>
2021-07-25 18:23:27 +0300

    refactor task 5: added logger

--- task01-linear-algorithms/src/main/java/by/epam/linear/model/AverageFinder.java
+++ task01-linear-algorithms/src/main/java/by/epam/linear/model/AverageFinder.java
@@ -1,11 +1,19 @@
 package by.epam.linear.model;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class AverageFinder {
+
+    private static final Logger logger = LogManager.getLogger(AverageFinder.class);
 
     /*
      * 5. Create an algorithm for finding the average of two numbers
      */
     public double average(double firstNumber, double secondNumber) {
-        return (firstNumber + secondNumber) / 2;
+        logger.debug("first number = {}, second number = {}", firstNumber, secondNumber);
+        double result = (firstNumber + secondNumber) / 2;
+        logger.debug("result = {}", result);
+        return result;
     }
 }
