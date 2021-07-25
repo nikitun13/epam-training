4f168693b7c11f00f28453e188d10d6e39344342
Nikita Romanov <nikitun13@gmail.com>
2021-07-25 19:49:41 +0300

    refactor task15

---
+++ task01-linear-algorithms/src/main/java/by/epam/linear/model/PowerCalculator.java
@@ -0,0 +1,19 @@
+package by.epam.linear.model;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class PowerCalculator {
+
+    private static final Logger logger = LogManager.getLogger(PowerCalculator.class);
+
+    /*
+     * 15. Write program, which prints first four powers of π.
+     */
+    public double powPi(double exponent) {
+        logger.debug("exponent = {}", exponent);
+        double result = Math.pow(Math.PI, exponent);
+        logger.debug("result = {}", result);
+        return result;
+    }
+}
