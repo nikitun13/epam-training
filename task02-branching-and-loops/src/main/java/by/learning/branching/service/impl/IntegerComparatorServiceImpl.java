6d3523ca2f52ef1fef1351483507a6f25360ce6e
Nikita Romanov <nikitun13@gmail.com>
2021-07-31 14:58:45 +0300

    add implementation for IntegerComparatorService

--- task02-branching-and-loops/src/main/java/by/learning/branching/service/impl/IntegerComparatorServiceImpl.java
+++ task02-branching-and-loops/src/main/java/by/learning/branching/service/impl/IntegerComparatorServiceImpl.java
@@ -1,11 +1,18 @@
 package by.learning.branching.service.impl;
 
 import by.learning.branching.service.IntegerComparatorService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class IntegerComparatorServiceImpl implements IntegerComparatorService {
 
+    private static final Logger logger = LogManager.getLogger(IntegerComparatorServiceImpl.class);
+
     @Override
     public int min(int a, int b) {
-        return 0; // stub
+        logger.debug("received: a = {}, b = {}", a, b);
+        int result = a <= b ? a : b;
+        logger.debug("result: {}", result);
+        return result;
     }
 }
