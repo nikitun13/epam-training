726203290ca457871eff5c2ef9688307272f8ee8
Nikita Romanov <nikitun13@gmail.com>
2021-07-31 14:46:20 +0300

    add IntegerComparator service implementation stub for TDD

---
+++ task02-branching-and-loops/src/main/java/by/learning/branching/service/impl/IntegerComparatorServiceImpl.java
@@ -0,0 +1,11 @@
+package by.learning.branching.service.impl;
+
+import by.learning.branching.service.IntegerComparatorService;
+
+public class IntegerComparatorServiceImpl implements IntegerComparatorService {
+
+    @Override
+    public int min(int a, int b) {
+        return 0; // stub
+    }
+}
