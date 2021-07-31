2cb3acae0a2df8a65ceb111c5fc61e4fd33c9bd0
Nikita Romanov <nikitun13@gmail.com>
2021-07-31 14:42:27 +0300

    add interface for IntegerComparator service (branching, task 5)

---
+++ task02-branching-and-loops/src/main/java/by/learning/branching/service/IntegerComparatorService.java
@@ -0,0 +1,19 @@
+package by.learning.branching.service;
+
+/**
+ * Describes the interface of a service that provides an integer comparison.
+ *
+ * @author Nikita Romanov
+ */
+public interface IntegerComparatorService {
+
+    /**
+     * Returns the smallest of two integers.
+     * If the integers have the same value, the result is that same value.
+     *
+     * @param a first number.
+     * @param b second number.
+     * @return the smallest of {@code a} and {@code b}.
+     */
+    int min(int a, int b);
+}
