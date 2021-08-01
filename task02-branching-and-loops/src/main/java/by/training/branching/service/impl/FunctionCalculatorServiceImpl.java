38793b1baa9ac8b2fc96270a658946e1896516f3
Nikita Romanov <nikitun13@gmail.com>
2021-08-01 19:52:34 +0300

    add implementation for FunctionCalculator service

--- task02-branching-and-loops/src/main/java/by/training/branching/service/impl/FunctionCalculatorServiceImpl.java
+++ task02-branching-and-loops/src/main/java/by/training/branching/service/impl/FunctionCalculatorServiceImpl.java
@@ -1,6 +1,8 @@
 package by.training.branching.service.impl;
 
 import by.training.branching.service.FunctionCalculatorService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * The class {@code FunctionCalculatorServiceImpl} is a class that implements {@link FunctionCalculatorService}.
@@ -11,8 +13,24 @@ import by.training.branching.service.FunctionCalculatorService;
  */
 public class FunctionCalculatorServiceImpl implements FunctionCalculatorService {
 
+    private static final Logger logger = LogManager.getLogger(FunctionCalculatorServiceImpl.class);
+
+    private static final int BOUND_OF_THE_FIRST_SCENARIO = 13;
+    private static final int THIRD_POWER = 3;
+    private static final int FREE_TERM_OF_THE_FUNCTION = 9;
+    private static final int COEFFICIENT_OF_THE_FUNCTION = -3;
+    private static final int FUNCTION_MEMBER = 1;
+
     @Override
     public double calculate(double x) {
-        return 0; // stub
+        logger.debug("received x = {}", x);
+        double result;
+        if (x <= BOUND_OF_THE_FIRST_SCENARIO) {
+            result = -Math.pow(x, THIRD_POWER) + FREE_TERM_OF_THE_FUNCTION;
+        } else {
+            result = COEFFICIENT_OF_THE_FUNCTION / (x + FUNCTION_MEMBER);
+        }
+        logger.debug("result: {}", result);
+        return result;
     }
 }
