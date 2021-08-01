ef224cd5ee4205b937143c532a2ed925e7017a08
Nikita Romanov <nikitun13@gmail.com>
2021-08-01 11:25:19 +0300

    add FunctionCalculator service implementation stub for TDD

---
+++ task02-branching-and-loops/src/main/java/by/training/branching/service/impl/FunctionCalculatorServiceImpl.java
@@ -0,0 +1,17 @@
+package by.training.branching.service.impl;
+
+import by.training.branching.service.FunctionCalculatorService;
+
+/**
+ * The class {@code FunctionCalculatorServiceImpl} is a class that implements {@link FunctionCalculatorService}.
+ * If {@code x <= 13}, calculates function {@code F(x) = -x^3 + 9}. If {@code x > 13}, calculates {@code F(x) = -(3 / (x + 1)}.
+ *
+ * @author Nikita Romanov
+ */
+public class FunctionCalculatorServiceImpl implements FunctionCalculatorService {
+
+    @Override
+    public double calculate(int x) {
+        return 0; // stub
+    }
+}
