53272a5e43af4f012679e0248dc65a0dfa976694
Nikita Romanov <nikitun13@gmail.com>
2021-08-02 15:27:33 +0300

    add implementation for Divisor service

--- task02-branching-and-loops/src/main/java/by/training/branching/service/impl/DivisorServiceImpl.java
+++ task02-branching-and-loops/src/main/java/by/training/branching/service/impl/DivisorServiceImpl.java
@@ -1,13 +1,29 @@
 package by.training.branching.service.impl;
 
 import by.training.branching.service.DivisorService;
+import by.training.branching.service.ServiceException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
+import java.util.Objects;
 
 public class DivisorServiceImpl implements DivisorService {
 
+    private static final Logger logger = LogManager.getLogger(DivisorServiceImpl.class);
+
     @Override
     public List<Integer> findDivisibleNumbers(int divisor, List<Integer> numbers) {
-        return null; // stub
+        logger.debug("received: divisor = {}, numbers: {}", divisor, numbers);
+        if (divisor == 0) {
+            throw new ServiceException("divisor can't be 0");
+        }
+        Objects.requireNonNull(numbers);
+        numbers.forEach(Objects::requireNonNull);
+        List<Integer> result = numbers.stream()
+                .filter(number -> number % divisor == 0)
+                .toList();
+        logger.debug("result: {}", result);
+        return result;
     }
 }
