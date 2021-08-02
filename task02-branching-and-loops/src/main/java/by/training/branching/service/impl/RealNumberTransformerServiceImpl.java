92e7659d5e4684a5e91d193dfd481173301e0cc3
Nikita Romanov <nikitun13@gmail.com>
2021-08-02 13:17:47 +0300

    add implementation for RealNumberTransformer service

--- task02-branching-and-loops/src/main/java/by/training/branching/service/impl/RealNumberTransformerServiceImpl.java
+++ task02-branching-and-loops/src/main/java/by/training/branching/service/impl/RealNumberTransformerServiceImpl.java
@@ -1,6 +1,9 @@
 package by.training.branching.service.impl;
 
 import by.training.branching.service.RealNumberTransformerService;
+import by.training.branching.service.ServiceException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
@@ -11,13 +14,46 @@ import java.util.List;
  */
 public class RealNumberTransformerServiceImpl implements RealNumberTransformerService {
 
+    private static final Logger logger = LogManager.getLogger(RealNumberTransformerServiceImpl.class);
+
+    private static final int MULTIPLICATION_COEFFICIENT = 2;
+
     @Override
     public List<Double> transformTwoDifferentRealNumbers(double a, double b) {
-        return null; // stub
+        logger.debug("received a = {}, b = {}", a, b);
+        if (a == b) {
+            throw new ServiceException("'a' can't be equal to 'b'");
+        }
+        double lesser = (a + b) / 2;
+        double greater = MULTIPLICATION_COEFFICIENT * a * b;
+        List<Double> result;
+        if (a > b) {
+            result = List.of(greater, lesser);
+        } else {
+            result = List.of(lesser, greater);
+        }
+        logger.debug("result: {}", result);
+        return result;
     }
 
     @Override
     public List<Double> transformThreeRealNumbers(double a, double b, double c) {
-        return null; // stub
+        logger.debug("received a = {}, b = {}, c = {}", a, b, c);
+        List<Double> result;
+        if (a > b && b > c) {
+            result = List.of(
+                    a * MULTIPLICATION_COEFFICIENT,
+                    b * MULTIPLICATION_COEFFICIENT,
+                    c * MULTIPLICATION_COEFFICIENT
+            );
+        } else {
+            result = List.of(
+                    Math.abs(a),
+                    Math.abs(b),
+                    Math.abs(c)
+            );
+        }
+        logger.debug("result: {}", result);
+        return result;
     }
 }
