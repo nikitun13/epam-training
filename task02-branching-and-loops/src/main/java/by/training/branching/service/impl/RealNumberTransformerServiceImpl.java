d38471d074cf11f151d372381f930cb7d534b81a
Nikita Romanov <nikitun13@gmail.com>
2021-08-02 11:29:08 +0300

    add RealNumberTransformer service implementation stub for TDD

---
+++ task02-branching-and-loops/src/main/java/by/training/branching/service/impl/RealNumberTransformerServiceImpl.java
@@ -0,0 +1,23 @@
+package by.training.branching.service.impl;
+
+import by.training.branching.service.RealNumberTransformerService;
+
+import java.util.List;
+
+/**
+ * The class {@code RealNumberTransformerServiceImpl} is a class that implements {@link RealNumberTransformerService}.
+ *
+ * @author Nikita Romanov
+ */
+public class RealNumberTransformerServiceImpl implements RealNumberTransformerService {
+
+    @Override
+    public List<Double> transformTwoDifferentRealNumbers(double a, double b) {
+        return null; // stub
+    }
+
+    @Override
+    public List<Double> transformThreeRealNumbers(double a, double b, double c) {
+        return null; // stub
+    }
+}
