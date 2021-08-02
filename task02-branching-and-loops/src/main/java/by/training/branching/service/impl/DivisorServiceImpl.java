57f45a6d811d731e7a64e2f784f6c35015cd3f9f
Nikita Romanov <nikitun13@gmail.com>
2021-08-02 14:04:53 +0300

    add Divisor service implementation stub for TDD

---
+++ task02-branching-and-loops/src/main/java/by/training/branching/service/impl/DivisorServiceImpl.java
@@ -0,0 +1,13 @@
+package by.training.branching.service.impl;
+
+import by.training.branching.service.DivisorService;
+
+import java.util.List;
+
+public class DivisorServiceImpl implements DivisorService {
+
+    @Override
+    public List<Integer> findDivisibleNumbers(int divisor, List<Integer> numbers) {
+        return null; // stub
+    }
+}
