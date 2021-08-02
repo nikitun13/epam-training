e24334ac79c0e44e62b76b5cda70a59a8d79825d
Nikita Romanov <nikitun13@gmail.com>
2021-08-02 14:03:02 +0300

    add interface for Divisor service (branching, task 20)

---
+++ task02-branching-and-loops/src/main/java/by/training/branching/service/DivisorService.java
@@ -0,0 +1,23 @@
+package by.training.branching.service;
+
+import java.util.List;
+
+/**
+ * Describes the interface of a service that finds numbers that are divisible by a certain divisor.
+ *
+ * @author Nikita Romanov
+ */
+public interface DivisorService {
+
+    /**
+     * All elements of {@code numbers} are divided by a {@code divisor}.
+     * Returns list of numbers that can be divided by a {@code divisor}.
+     *
+     * @param divisor divisor of numbers
+     * @param numbers list of numbers
+     * @return list of numbers that can be divided by a {@code divisor}.
+     * @throws ServiceException     if {@code divisor} equals {@code 0}.
+     * @throws NullPointerException if {@code numbers} is null.
+     */
+    List<Integer> findDivisibleNumbers(int divisor, List<Integer> numbers);
+}
