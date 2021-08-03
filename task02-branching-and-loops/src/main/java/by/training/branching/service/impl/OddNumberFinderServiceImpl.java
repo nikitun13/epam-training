5f46c815a0ec294680c0306e94516f571c7761c8
Nikita Romanov <nikitun13@gmail.com>
2021-08-03 12:31:37 +0300

    add implementation for OddNumberFinder service

--- task02-branching-and-loops/src/main/java/by/training/branching/service/impl/OddNumberFinderServiceImpl.java
+++ task02-branching-and-loops/src/main/java/by/training/branching/service/impl/OddNumberFinderServiceImpl.java
@@ -1,13 +1,32 @@
 package by.training.branching.service.impl;
 
 import by.training.branching.service.OddNumberFinderService;
+import by.training.branching.service.ServiceException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OddNumberFinderServiceImpl implements OddNumberFinderService {
 
+    private static final Logger logger = LogManager.getLogger(OddNumberFinderServiceImpl.class);
+
     @Override
     public List<Integer> findOddNumbersInRange(int lowerBound, int upperBound) {
-        return null; // stub
+        logger.debug("received: lowerBound = {}, upperBound = {}", lowerBound, upperBound);
+        if (lowerBound > upperBound) {
+            throw new ServiceException("lower bound is greater than upper bound");
+        }
+        List<Integer> result = new ArrayList<>();
+        if (lowerBound % 2 == 0) {
+            lowerBound++;
+        }
+        while (lowerBound <= upperBound) {
+            result.add(lowerBound);
+            lowerBound += 2;
+        }
+        logger.debug("result: {}", result);
+        return result;
     }
 }
