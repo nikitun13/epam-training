f31e8caa26cd453a94b2fcc528f7b9d35f740f6d
Nikita Romanov <nikitun13@gmail.com>
2021-08-03 13:04:48 +0300

    add RomanInteger entity

---
+++ task02-branching-and-loops/src/main/java/by/training/branching/entity/RomanInteger.java
@@ -0,0 +1,43 @@
+package by.training.branching.entity;
+
+import java.util.Objects;
+
+/**
+ * The class {@code RomanInteger} is a class that represents a roman number.
+ *
+ * @author Nikita Romanov
+ */
+public class RomanInteger {
+
+    private String value;
+
+    public RomanInteger(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RomanInteger that = (RomanInteger) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
