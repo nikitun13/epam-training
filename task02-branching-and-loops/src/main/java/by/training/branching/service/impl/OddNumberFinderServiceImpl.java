bd91434576104ca5bb70da1b75e3656f60471183
Nikita Romanov <nikitun13@gmail.com>
2021-08-03 12:00:09 +0300

    add OddNumberFinder service implementation stub for TDD

---
+++ task02-branching-and-loops/src/main/java/by/training/branching/service/impl/OddNumberFinderServiceImpl.java
@@ -0,0 +1,13 @@
+package by.training.branching.service.impl;
+
+import by.training.branching.service.OddNumberFinderService;
+
+import java.util.List;
+
+public class OddNumberFinderServiceImpl implements OddNumberFinderService {
+
+    @Override
+    public List<Integer> findOddNumbersInRange(int lowerBound, int upperBound) {
+        return null; // stub
+    }
+}
