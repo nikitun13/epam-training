3090699f63edafb222e5d9e6a5d9712c7937fbaf
Nikita Romanov <nikitun13@gmail.com>
2021-08-03 13:52:48 +0300

    add RomanNumbers service implementation stub for TDD

---
+++ task02-branching-and-loops/src/main/java/by/training/branching/service/impl/RomanNumbersServiceImpl.java
@@ -0,0 +1,17 @@
+package by.training.branching.service.impl;
+
+import by.training.branching.entity.RomanInteger;
+import by.training.branching.service.RomanNumbersService;
+
+/**
+ * The class {@code RomanNumbersServiceImpl} is a class that implements {@link RomanNumbersService}.
+ *
+ * @author Nikita Romanov
+ */
+public class RomanNumbersServiceImpl implements RomanNumbersService {
+
+    @Override
+    public int fromRoman(RomanInteger romanNumber) {
+        return 0; // stub
+    }
+}
