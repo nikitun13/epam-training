25df2e910bd6dfa9057697c101e3cdedc7812a11
Nikita Romanov <nikitun13@gmail.com>
2021-08-04 10:16:17 +0300

    add NumberSeries service implementation stub for TDD

---
+++ task02-branching-and-loops/src/main/java/by/training/branching/service/impl/NumberSeriesServiceImpl.java
@@ -0,0 +1,16 @@
+package by.training.branching.service.impl;
+
+import by.training.branching.service.NumberSeriesService;
+
+public class NumberSeriesServiceImpl implements NumberSeriesService {
+
+    @Override
+    public long calculatePowersOfTwo(int power) {
+        return 0; // stub
+    }
+
+    @Override
+    public double calculateNumberSeries(double e) {
+        return 0; //stub
+    }
+}
