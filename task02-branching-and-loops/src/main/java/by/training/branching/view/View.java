d5beb387221c6702378e8e03b2b93a3461794f1e
Nikita Romanov <nikitun13@gmail.com>
2021-08-05 11:10:54 +0300

    Add View interface and Console view implementation

---
+++ task02-branching-and-loops/src/main/java/by/training/branching/view/View.java
@@ -0,0 +1,37 @@
+package by.training.branching.view;
+
+/**
+ * Describes the interface of a view for user interaction.
+ *
+ * @author Nikita Romanov
+ */
+public interface View {
+
+    /**
+     * Prints {@code message} for user.
+     * Adds a line break to the end of the {@code message}.
+     *
+     * @param message message for user.
+     */
+    void println(String message);
+
+    /**
+     * Prints {@code message} for user.
+     * Doesn't add a line break to the end of the {@code message}.
+     *
+     * @param message message for user.
+     */
+    void print(String message);
+
+    /**
+     * Adds a line break.
+     */
+    void printNewLine();
+
+    /**
+     * Reads a line from user input.
+     *
+     * @return user input.
+     */
+    String read();
+}
