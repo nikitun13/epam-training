3f3dbad7ff48d3b5e6e97c53a87764dce0f016ab
Nikita Romanov <nikitun13@gmail.com>
2021-08-05 15:20:16 +0300

    add CommandResult

---
+++ task02-branching-and-loops/src/main/java/by/training/branching/controller/command/result/CommandStatus.java
@@ -0,0 +1,14 @@
+package by.training.branching.controller.command.result;
+
+/**
+ * The class {@code CommandStatus} is an enumeration
+ * of possible statuses of the command execution result.
+ *
+ * @author Nikita Romanov
+ * @see CommandResult
+ */
+public enum CommandStatus {
+    OK,
+    ERROR,
+    EXIT
+}
