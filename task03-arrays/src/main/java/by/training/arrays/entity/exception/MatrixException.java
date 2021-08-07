1b2ffbbd3e30f500912f44c139affad73e2fb54d
Nikita Romanov <nikitun13@gmail.com>
2021-08-07 14:08:36 +0300

    add MatrixException

---
+++ task03-arrays/src/main/java/by/training/arrays/entity/exception/MatrixException.java
@@ -0,0 +1,19 @@
+package by.training.arrays.entity.exception;
+
+public class MatrixException extends RuntimeException {
+
+    public MatrixException() {
+    }
+
+    public MatrixException(String message) {
+        super(message);
+    }
+
+    public MatrixException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MatrixException(Throwable cause) {
+        super(cause);
+    }
+}
