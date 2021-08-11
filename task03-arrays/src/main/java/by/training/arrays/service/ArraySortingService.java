0639f4d90329f33d1ddc991fc4f11640e3e94bda
Nikita Romanov <nikitun13@gmail.com>
2021-08-11 12:45:18 +0300

    update ArraySorting service

---
+++ task03-arrays/src/main/java/by/training/arrays/service/ArraySortingService.java
@@ -0,0 +1,26 @@
+package by.training.arrays.service;
+
+import by.training.arrays.entity.Array;
+
+/**
+ * Describes the interface of a service
+ * that sorts {@link Array} of {@code comparable} elements.
+ *
+ * @author Nikita Romanov
+ * @see Array
+ * @see Comparable
+ */
+@FunctionalInterface
+public interface ArraySortingService {
+
+    /**
+     * Sorts elements using comparing method
+     * from {@link Comparable} interface.<br>
+     * The sorting type depends
+     * on the specific implementation.
+     *
+     * @param array {@code Array} of elements for sorting.
+     * @param <T>   type of element for sorting.
+     */
+    <T extends Comparable<? super T>> void sort(Array<T> array);
+}
