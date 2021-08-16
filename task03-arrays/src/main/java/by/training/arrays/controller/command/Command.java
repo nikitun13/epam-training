a16f74d46319c5e6d14766b9fe3423f0e4961236
Nikita Romanov <nikitun13@gmail.com>
2021-08-16 15:02:41 +0300

    add Command interface

---
+++ task03-arrays/src/main/java/by/training/arrays/controller/command/Command.java
@@ -0,0 +1,21 @@
+package by.training.arrays.controller.command;
+
+import by.training.arrays.controller.command.result.CommandResult;
+
+/**
+ * Describes the interface of a command that executes task.
+ *
+ * @author Nikita Romanov
+ */
+@FunctionalInterface
+public interface Command {
+
+    /**
+     * Executes task.
+     *
+     * @param params array of string parameters.
+     * @return {@code CommandResult} that contains status and message.
+     * @see CommandResult
+     */
+    CommandResult execute(String[] params);
+}
