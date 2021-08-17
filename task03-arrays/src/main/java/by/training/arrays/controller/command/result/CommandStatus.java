f7fc9c22e186e4a32b5cca82403a4934d039b1b7
Nikita Romanov <nikitun13@gmail.com>
2021-08-17 11:16:59 +0300

    add CommandResult

---
+++ task03-arrays/src/main/java/by/training/arrays/controller/command/result/CommandStatus.java
@@ -0,0 +1,16 @@
+package by.training.arrays.controller.command.result;
+
+/**
+ * The class {@code CommandStatus} is an enumeration
+ * of possible statuses of the command execution result.
+ *
+ * @author Nikita Romanov
+ * @see CommandResult
+ */
+public enum CommandStatus {
+    OK,
+    INVALID_NUMBER_OF_PARAMETERS,
+    INVALID_INPUT_PARAMETERS,
+    ERROR,
+    EXIT
+}
