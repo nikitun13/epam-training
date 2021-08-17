daef1ae31c1d86dd21c93dc5ba06e312880e9f71
Nikita Romanov <nikitun13@gmail.com>
2021-08-17 12:09:57 +0300

    add ExitCommand

---
+++ task03-arrays/src/main/java/by/training/arrays/controller/command/impl/ExitCommand.java
@@ -0,0 +1,31 @@
+package by.training.arrays.controller.command.impl;
+
+import by.training.arrays.controller.command.Command;
+import by.training.arrays.controller.command.result.CommandResult;
+import by.training.arrays.controller.command.result.CommandStatus;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * The class {@code ExitCommand} is a class
+ * that implements {@link Command}.<br/>
+ * Executed when the command 'exit' is received.
+ * This command terminates the application.
+ *
+ * @author Nikita Romanov
+ */
+public class ExitCommand implements Command {
+
+    private static final Logger logger =
+            LogManager.getLogger(ExitCommand.class);
+
+    private final CommandResult exitResult =
+            new CommandResult(CommandStatus.EXIT);
+
+    @Override
+    public CommandResult execute(String[] params) {
+        logger.debug("Exit command is running");
+        logger.debug("result: {}", exitResult);
+        return exitResult;
+    }
+}
