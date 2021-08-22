84036ed8f93cbdd09367b6eb01bdd2b9f53dbfdb
Nikita Romanov <nikitun13@gmail.com>
2021-08-22 13:45:58 +0300

    add Word

---
+++ task04-classes/src/main/java/by/training/classes/entity/Word.java
@@ -0,0 +1,38 @@
+package by.training.classes.entity;
+
+import java.util.Objects;
+
+public class Word {
+
+    private String value;
+
+    public Word(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Word word1 = (Word) o;
+        return Objects.equals(value, word1.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
