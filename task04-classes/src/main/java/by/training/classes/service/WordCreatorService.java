3081c4aedb58b29dfbc26ac8fe8efc4da316435e
Nikita Romanov <nikitun13@gmail.com>
2021-08-23 20:17:42 +0300

    add WordCreator service

---
+++ task04-classes/src/main/java/by/training/classes/service/WordCreatorService.java
@@ -0,0 +1,23 @@
+package by.training.classes.service;
+
+import by.training.classes.entity.Word;
+
+/**
+ * Describes the interface of a service
+ * that creates {@link Word}.
+ *
+ * @author Nikita Romanov
+ * @see Word
+ */
+public interface WordCreatorService {
+
+    /**
+     * Creates {@link Word} using
+     * string value of this {@code word}.
+     *
+     * @param value string value of the {@link Word}.
+     * @return new {@code Word} from {@code value}.
+     * @throws ServiceException if {@code value} is null.
+     */
+    Word createWord(String value) throws ServiceException;
+}
