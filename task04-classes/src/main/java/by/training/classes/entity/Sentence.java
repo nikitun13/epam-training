7d69c9f4edce2535959d21ad3d02519aa3593079
Nikita Romanov <nikitun13@gmail.com>
2021-08-25 14:29:30 +0300

    update Sentence

--- task04-classes/src/main/java/by/training/classes/entity/Sentence.java
+++ task04-classes/src/main/java/by/training/classes/entity/Sentence.java
@@ -30,6 +30,10 @@ public class Sentence {
         words.add(word);
     }
 
+    public int getNumberOfWords() {
+        return words.size();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
