b2dfd49cc1bcc0a4d408ae0b30f72bd59590852a
Nikita Romanov <nikitun13@gmail.com>
2021-08-26 11:19:24 +0300

    add Controller

---
+++ task04-classes/src/main/java/by/training/classes/controller/Runner.java
@@ -0,0 +1,25 @@
+package by.training.classes.controller;
+
+import by.training.classes.view.ConsoleView;
+import by.training.classes.view.View;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * The class {@code Runner} is a main class
+ * that launches the application.
+ *
+ * @author Nikita Romanov
+ */
+public class Runner {
+
+    private static final Logger logger = LogManager.getLogger(Runner.class);
+
+    public static void main(String[] args) {
+        logger.info("application started");
+        View view = new ConsoleView();
+        Controller controller = new ControllerImpl(view);
+        controller.run();
+        logger.info("application finished");
+    }
+}
