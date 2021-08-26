30a5086b520054460ca69d0a0586020d806d7056
Nikita Romanov <nikitun13@gmail.com>
2021-08-26 11:05:13 +0300

    add View interface and its implementation

---
+++ task04-classes/src/main/java/by/training/classes/view/ConsoleView.java
@@ -0,0 +1,34 @@
+package by.training.classes.view;
+
+import java.util.Scanner;
+
+/**
+ * The class {@code ConsoleView} is a class that implements {@link View}.
+ * Provides user interaction through the console.
+ *
+ * @author Nikita Romanov
+ */
+public class ConsoleView implements View {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public void println(String message) {
+        System.out.println(message);
+    }
+
+    @Override
+    public void print(String message) {
+        System.out.print(message);
+    }
+
+    @Override
+    public void printNewLine() {
+        System.out.println();
+    }
+
+    @Override
+    public String read() {
+        return scanner.nextLine();
+    }
+}
