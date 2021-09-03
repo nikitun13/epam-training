7a79574ce10ff32f3b2151ea11ce4447f27b42df
Nikita Romanov <nikitun13@gmail.com>
2021-09-03 14:21:09 +0300

    add ex03 - Thread priority

---
+++ demo-threads/src/main/java/by/training/thread/ex03threadpriority/PriorThread.java
@@ -0,0 +1,26 @@
+package by.training.thread.ex03threadpriority;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class PriorThread extends Thread {
+
+    private static final Logger log
+            = LogManager.getLogger(PriorThread.class);
+
+    public PriorThread(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 50; i++) {
+            log.info(getName() + " " + i);
+            try {
+                Thread.sleep(0); // try sleep(1),sleep(0),sleep(10)
+            } catch (InterruptedException e) {
+                log.error(e);
+            }
+        }
+    }
+}
