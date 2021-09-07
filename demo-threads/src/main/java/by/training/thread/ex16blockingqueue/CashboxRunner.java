f97f3d7c2e3aad1863113afc57081fa53309d9f5
Nikita Romanov <nikitun13@gmail.com>
2021-09-07 14:16:45 +0300

    add ex16 - Blocking Queue

---
+++ demo-threads/src/main/java/by/training/thread/ex16blockingqueue/CashboxRunner.java
@@ -0,0 +1,30 @@
+package by.training.thread.ex16blockingqueue;
+
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CashboxRunner {
+
+    public static void main(String[] args) throws InterruptedException {
+        BlockingQueue<Cashbox> cashboxes = new ArrayBlockingQueue<>(2, true, List.of(new Cashbox(), new Cashbox()));
+        List<Thread> threads = Stream.of(
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes)
+                )
+                .map(Thread::new)
+                .peek(Thread::start)
+                .collect(Collectors.toList());
+        for (Thread thread : threads) {
+            thread.join();
+        }
+    }
+}
