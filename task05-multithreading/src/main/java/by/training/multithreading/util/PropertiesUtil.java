f02840513a8a855524f51bca56a5c4498a940e40
Nikita Romanov <nikitun13@gmail.com>
2021-09-11 09:35:31 +0300

    add PropertiesUtil

---
+++ task05-multithreading/src/main/java/by/training/multithreading/util/PropertiesUtil.java
@@ -0,0 +1,29 @@
+package by.training.multithreading.util;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public final class PropertiesUtil {
+
+    private static final Properties PROPERTIES = new Properties();
+
+    static {
+        loadProperties();
+    }
+
+    private PropertiesUtil() {
+    }
+
+    private static void loadProperties() {
+        try (var inputStream = PropertiesUtil.class.getClassLoader()
+                .getResourceAsStream("application.properties")) {
+            PROPERTIES.load(inputStream);
+        } catch (IOException e) {
+            throw new UtilException(e);
+        }
+    }
+
+    public static String getProperty(String key) {
+        return PROPERTIES.getProperty(key);
+    }
+}
---
+++ task05-multithreading/src/main/java/by/training/multithreading/util/UtilException.java
@@ -0,0 +1,19 @@
+package by.training.multithreading.util;
+
+public class UtilException extends RuntimeException {
+
+    public UtilException() {
+    }
+
+    public UtilException(String message) {
+        super(message);
+    }
+
+    public UtilException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UtilException(Throwable cause) {
+        super(cause);
+    }
+}
