aad983b07902ea6022caf31f8f698feafea0beeb
Nikita Romanov <nikitun13@gmail.com>
2021-09-19 15:08:02 +0300

    add Axis

---
+++ task06-shapes/src/main/java/by/training/shapes/entity/Axis.java
@@ -0,0 +1,23 @@
+package by.training.shapes.entity;
+
+/**
+ * The class {@code Axis} is an enumeration
+ * of coordinate axes.
+ *
+ * @author Nikita Romanov
+ */
+public enum Axis {
+
+    /**
+     * Represent X-axis.
+     */
+    X_AXIS,
+    /**
+     * Represent Y-axis.
+     */
+    Y_AXIS,
+    /**
+     * Represent Z-axis.
+     */
+    Z_AXIS
+}
