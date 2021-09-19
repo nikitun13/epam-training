6ab5c6d066536a422f73a319de40a71316ad7799
Nikita Romanov <nikitun13@gmail.com>
2021-09-19 16:20:47 +0300

    add BallCalculatorService

---
+++ task06-shapes/src/main/java/by/training/shapes/service/BallCalculatorService.java
@@ -0,0 +1,28 @@
+package by.training.shapes.service;
+
+import by.training.shapes.entity.Ball;
+import by.training.shapes.entity.Plane;
+
+/**
+ * Describes the interface of a service
+ * that extends {@link VolumetricShapeCalculatorService}.
+ *
+ * @author Nikita Romanov
+ * @see VolumetricShapeCalculatorService
+ */
+public interface BallCalculatorService
+        extends VolumetricShapeCalculatorService<Ball> {
+
+    /**
+     * Returns the ratio of the volumes of the parts
+     * dissected by the coordinate {@link Plane}.
+     *
+     * @param ball  ball to be dissected.
+     * @param plane coordinate plane for dissection.
+     * @return ratio of the parts.
+     * @throws ServiceException if {@code ball} is invalid
+     *                          or {@code plane} is {@code null}.l
+     */
+    double calculateRatioOfPartsDissectedByPlane(Ball ball, Plane plane)
+            throws ServiceException;
+}
