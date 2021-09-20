95816c5f23076cc9625f92243adeeb19c3957990
Nikita Romanov <nikitun13@gmail.com>
2021-09-20 19:00:36 +0300

    add DaoException

---
+++ task06-shapes/src/main/java/by/training/shapes/dao/DaoException.java
@@ -0,0 +1,26 @@
+package by.training.shapes.dao;
+
+/**
+ * The class {@code DaoException}
+ * is a class that implements {@link Exception}.<br/>
+ * This exception is thrown by dao classes.
+ *
+ * @author Nikita Romanov
+ */
+public class DaoException extends Exception {
+
+    public DaoException() {
+    }
+
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DaoException(Throwable cause) {
+        super(cause);
+    }
+}
