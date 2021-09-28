7f8181bbedb17bdbf3ad25d48d47376130e4893b
Nikita Romanov <nikitun13@gmail.com>
2021-09-28 14:55:30 +0300

    refactor

---
+++ task07-information-handling/src/main/java/by/training/information/dao/parser/StringToTextComponentsParser.java
@@ -0,0 +1,27 @@
+package by.training.information.dao.parser;
+
+import by.training.information.entity.TextComponent;
+
+import java.util.List;
+
+/**
+ * Describes interface of a parser that parses
+ * string to {@code TextComponents}.
+ *
+ * @param <T> concrete type of {@link TextComponent}.
+ * @author Nikita Romanov
+ * @see TextComponent
+ */
+@FunctionalInterface
+public interface StringToTextComponentsParser<T extends TextComponent> {
+
+    /**
+     * Parses {@code input} string to {@code TextComponents}.<br/>
+     * Parsing result depends on the concrete {@code Parser}.
+     *
+     * @param input string to be parsed.
+     * @return parsed list of {@code T}.
+     * @see TextComponent
+     */
+    List<T> parse(String input);
+}
