561a5abe0d1886ec06b1fe6914883a3b733be983
Nikita Romanov <nikitun13@gmail.com>
2021-10-11 12:04:58 +0300

    add View

---
+++ task08-xml/src/main/java/by/training/xml/view/ConsoleView.java
@@ -0,0 +1,37 @@
+package by.training.xml.view;
+
+import java.util.Scanner;
+
+/**
+ * The class {@code ConsoleView} is a class that implements {@link View}.
+ * Provides user interaction through the console.
+ *
+ * @author Nikita Romanov
+ */
+public class ConsoleView implements View {
+
+    /**
+     * {@link Scanner} for reading from the console.
+     */
+    private final Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public void println(final String message) {
+        System.out.println(message);
+    }
+
+    @Override
+    public void print(final String message) {
+        System.out.print(message);
+    }
+
+    @Override
+    public void printNewLine() {
+        System.out.println();
+    }
+
+    @Override
+    public String read() {
+        return scanner.nextLine();
+    }
+}
